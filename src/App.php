9a3335a6f8fb9e4bafcb66befc15656982e5ad36
kassle <kassle@krybrig.org>
2023-08-06 20:46:58 +0700

    Implement get enroled users from course

--- src/App.php
+++ src/App.php
@@ -6,4 +6,23 @@ interface App {
     public function getUserById(string $id) : array;
     public function getUserByUsername(string $username) : array;
     public function getUserByEmail(string $email) : array;
+
+    /* blocked
+    "Access to the function core_user_update_users() is not allowed.
+    There could be multiple reasons for this:
+    1. The service linked to the user token does not contain the function.
+    2. The service is user-restricted and the user is not listed.
+    3. The service is IP-restricted and the user IP is not listed.
+    4. The service is time-restricted and the time has expired.
+    5. The token is time-restricted and the time has expired.
+    6. The service requires a specific capability which the user does not have.
+    7. The function is called with username/password (no user token is sent) and none of the services has the function to allow the user.
+    These settings can be found in Administration > Site administration
+    > Server > Web services > External services and Manage tokens."
+    */
+    // public function updateUser(string $id, string $city) : mixed;
+    // public function enrolUserToCourse(string $courseId, string $userId) : mixed;
+    // public function unEnrolUserToCourse(string $courseId, string $userId) : mixed;
+
+    public function getEnroledUsersByCourseId(string $courseId) : array;
 }
--- src/AppImpl.php
+++ src/AppImpl.php
@@ -23,8 +23,8 @@ class AppImpl implements App {
         return $this->getUserByField("email", $email);
     }
 
-    private function getUserByField(string $field, string $value) {
-        $result = $this->rest->request('core_user_get_users_by_field', array("field" => $field, "values" => array($value)));
+    private function getUserByField(string $field, string $value) : array {
+        $result = $this->rest->request("core_user_get_users_by_field", array("field" => $field, "values" => array($value)));
 
         if (is_array($result)) {
             return $result;
@@ -32,4 +32,24 @@ class AppImpl implements App {
             return array();
         }
     }
+
+    // public function updateUser(string $id, string $city) : mixed {
+    //     $result = $this->rest->request("core_user_update_users", array("id" => $id, "city" => $city, MoodleRest::METHOD_POST));
+    //     return $result;
+    // }
+
+    public function getEnroledUsersByCourseId(string $courseId) : array {
+        $result = $this->rest->request("core_enrol_get_enrolled_users", array("courseid" => $courseId));
+
+        if (is_array($result)) {
+            return $result;
+        } else {
+            return array();
+        }
+    }
+
+    // public function enrolUserToCourse(string $courseId, string $userId) : mixed {
+    //     $result = $this->rest->request("core_enrol_submit_user_enrolment_form", array("courseid" => $courseId, "userId" => $userId), MoodleRest::METHOD_POST);
+    //     return $result;
+    // }
 }
