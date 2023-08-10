23b597408f54ba3d8339bdbb51b42ef499bfa1c2
kassle <kassle@krybrig.org>
2023-08-10 22:33:23 +0700

    added negative unit test for update user function

--- tests/AppImplTest.php
+++ tests/AppImplTest.php
@@ -122,6 +122,49 @@ class AppImplTest extends TestCase {
         return $rest;
     }
 
+    // public function testUpdateUserShouldReturnSuccess() {
+    //     $rest = $this->createMoodleRestMock();
+    //     $rest->expect()
+    // }
+
+    public function testUpdateUserShouldReturnError400WhenException() {
+        $rest = $this->createMoodleRestMockForRequestThatThrowException();
+        
+        $app = new AppImpl($rest);
+        $result = $app->updateUser("any", "", "", "", "", "", "us");
+
+        $this->assertEquals(0, sizeof($result["data"]));
+        $this->assertEquals(400, $result["error"]["code"]);
+    }
+
+    public function testUpdateUserShouldReturnError400WhenNoDataChanged() {
+        $rest = $this->createMoodleRestMock();
+        
+        $app = new AppImpl($rest);
+        $result = $app->updateUser("any", "", "", "", "", "", "");
+
+        $this->assertEquals(0, sizeof($result["data"]));
+        $this->assertEquals(400, $result["error"]["code"]);
+    }
+
+    public function testUpdateUserShouldReturnError500WhenResponseError() {
+        $response = array(
+            "errorcode" => "exception",
+            "message" => "access denied"
+        );
+        $rest = $this->createMoodleRestMock();
+        $rest->expects($this->once())
+            ->method("request")
+            ->willReturn($response);
+        
+        $app = new AppImpl($rest);
+        $result = $app->updateUser("any", "secret", "", "", "", "", "us");
+
+        $this->assertEquals(0, sizeof($result["data"]));
+        $this->assertEquals(500, $result["error"]["code"]);
+        $this->assertEquals($response["message"], $result["error"]["message"]);
+    }
+
     private function createMoodleRestMockForRequestThatThrowException() {
         $rest = $this->createMoodleRestMock();
         $rest->expects($this->once())
